2f14b99884dfad208be2fc76e8066742470fed29
floating-bottle <yann.baar@icloud.com>
2024-12-21 20:19:21 +0100

    Add Midpoint file.

--- GoToMidpoint/GoToMidpoint.ino
+++ GoToMidpoint/GoToMidpoint.ino
@@ -1,12 +1,3 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
@@ -23,4 +14,3 @@ void loop() {
   myservo.writeMicroseconds(1500);
   delay(2000);
 }
-
