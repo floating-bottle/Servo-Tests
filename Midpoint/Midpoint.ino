4693489957154aac12930018192a541bb4cc6188
floating-bottle <yann.baar@icloud.com>
2024-12-21 20:34:29 +0100

    Make Midpoint file

---
+++ Midpoint/Midpoint.ino
@@ -0,0 +1,14 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+void setup() {
+  myservo.attach(9, 500, 2500);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
+}
+
+void loop() {
+  myservo.writeMicroseconds(1500);
+  delay(2000);
+}
