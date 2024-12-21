ef0da1a64697a7a3a531a2166b2a9f84cd20871d
floating-bottle <yann.baar@icloud.com>
2024-12-21 19:42:58 +0100

    add pause && adapapt pwm

--- Servo-Sweep/Servo-Sweep.ino
+++ Servo-Sweep/Servo-Sweep.ino
@@ -15,7 +15,7 @@ Servo myservo;  // create servo object to control a servo
 int pos = 0;    // variable to store the servo position
 
 void setup() {
-  myservo.attach(9, 300, 2700);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(9, 500, 2500);  // attaches the servo on pin 9 to the servo object
   Serial.begin(9600);
 }
 
@@ -26,10 +26,12 @@ void loop() {
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
+  delay(500);
   for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
     Serial.println(pos);
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(15);                       // waits 15ms for the servo to reach the position
   }
+  delay(500);
 }
 
